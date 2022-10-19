6dfbc7f24744bdc2874cae2c3d9cf15d10bd6071
Christian Alexander <kakkoiichris@gmail.com>
2022-10-19 00:36:23 -0400

    Added the Main file.

---
+++ src/kakkoiichris/hyper/Main.kt
@@ -0,0 +1,27 @@
+/*************************************************************
+ * ______  __                                   __  /|  __    *
+ * ___  / / /_____  __________ _____ ________  / / / | |__\   *
+ * __  /_/ / __  / / /___  __ \_  _ \__  ___/ / / /  |__  |\  *
+ * _  __  /  _  /_/ / __  /_/ //  __/_  /    | / /__    / / | *
+ * /_/ /_/   _\__, /  _  .___/ \___/ /_/      \|__  |  / / /  *
+ *           /____/   /_/                      \__| | / /_/   *
+ *                                                  |/        *
+ *                  HYPER SCRIPTING LANGUAGE                  *
+ *          Copyright (C) 2018, Christian Alexander           *
+ **************************************************************/
+package kakkoiichris.hyper
+
+/**
+ * Hyper
+ 
+ * Copyright (C) 2022, KakkoiiChris
+ *
+ * File:    Main.kt
+ 
+ * Created: Wednesday, October 19, 2022, 00:34:13
+ *
+ * @author Christian Bryce Alexander
+ */
+fun main() {
+    println("HYPER (⚡)")
+}
